829cf57f48be3f2511e5f4b1699753584e905aee
Paolo Terrasi <pterrasi@6river.com>
2021-08-16 10:58:16 -0400

    Update csu.py

--- csu.py
+++ csu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-mtoken = "4"
+mtoken = "5e92ad4e3edd1843aeb25bbfeb8272008c0769278daf7d5ed2797e7c66e451d3"
 try:
    while True:
        print(mtoken)
